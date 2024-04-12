ecdb0ed7210192774b512b6a31165159031d6248
tuhinjamal <tuhinjamal1@gmail.com>
2024-04-13 00:44:23 +0600

    registration in progress

--- lecture-7/src/index.js
+++ lecture-7/src/index.js
@@ -5,7 +5,18 @@ dotenv.config({
   path: "./.env",
 });
 import connectDB from "./db/index.js";
-connectDB();
+import { app } from "./app.js";
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    {
+      console.log("MONGO db connection failed");
+    }
+  });
 
 // this is not a good way to make database connection
 /*import express from express;
